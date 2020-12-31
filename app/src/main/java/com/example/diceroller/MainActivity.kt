9c32d0c05a69f5500f718a9b6521b87adb7f5d51
Elizabeth Groton <elizabethgroton@Elizabeths-MacBook-Air.local>
2020-12-30 20:26:31 -0500

    feat: dice rolls randomly

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -16,12 +17,22 @@ class MainActivity : AppCompatActivity() {
             rollDice()
         }
     }
-    //random number generator
-    private fun rollDice() {
-       val resultText: TextView = findViewById(R.id.result_text)
 
+    private fun rollDice() {
+        //random number generator
         val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        //when statement dependent upon random int^
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        //setting the dice image depending on the when statement
+        diceImage.setImageResource(drawableResource)
     }
 
 }
